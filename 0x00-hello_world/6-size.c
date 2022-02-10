bbdbbd71e5f68e0e357029120d2fff84cc3849b9
viktorkrill <48968101+viktorkrill@users.noreply.github.com>
2022-02-10 15:27:46 -0500

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-printf("Side of a char: %d bytes\n", sizeof(char));
-printf("Size of an int: %d bytes\n", sizeof(int));
-printf("Size of a long int: %d byte\n", sizeof(long int));
-printf("Size of a long long int: %d byte\n", sizeof(long long int));
-printf("Size of a float: %d byte\n", sizeof(float));
+printf("Side of a char: %i bytes\n", sizeof(char));
+printf("Size of an int: %i bytess\n", sizeof(int));
+printf("Size of a long int: %i bytes\n", sizeof(long int));
+printf("Size of a long long int: %i bytes\n", sizeof(long long int));
+printf("Size of a float: %i bytes\n", sizeof(float));
 return (0);
 }
