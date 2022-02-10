ef39d7368ef82ff0e87f3fafe9672caa621ccc83
viktorkrill <48968101+viktorkrill@users.noreply.github.com>
2022-02-10 15:11:02 -0500

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main - print  variables
- * Return: 0 is value to return
+ * main - print values
+ * Return: 0 is value return
  */
 int main(void)
 {
 printf("Side of a char: %d bytes\n", sizeof(char));
 printf("Size of an int: %d bytes\n", sizeof(int));
 printf("Size of a long int: %d byte\n", sizeof(long int));
-printf("Sizeof a long long int: %d byte\n", sizeof(long long int));
+printf("Size of a long long int: %d byte\n", sizeof(long long int));
 printf("Size of a float: %d byte\n", sizeof(float));
 return (0);
 }
