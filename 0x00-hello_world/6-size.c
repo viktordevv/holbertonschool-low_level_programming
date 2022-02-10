6e032810d03be9db345ac315bfb6ed937bc48018
viktor krill <viktorkrilldj@gmail.com>
2022-02-10 15:08:48 -0800

    comiteando

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,15 @@
 */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
+char typeA;
+int typeB;
+long int tC;
+long long int tD;
 float z;
-printf("Side of a char: %i bytes\n", sizeof(a));
-printf("Size of an int: %i bytes\n", sizeof(b));
-printf("Size of a long int: %i bytes\n", sizeof(c));
-printf("Size of a long long int: %i bytes\n", sizeof(d));
+printf("Side of a char: %i bytes\n", sizeof(typeA));
+printf("Size of an int: %i bytes\n", sizeof(typeB));
+printf("Size of a long int: %i bytes\n", sizeof(tC));
+printf("Size of a long long int: %i bytes\n", sizeof(tD));
 printf("Size of a float: %i bytes\n", sizeof(z));
 return (0);
 }
