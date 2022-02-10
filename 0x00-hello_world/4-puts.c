e8a24ce8a7b75cfa235fbfaf352a8c65152a1c93
viktorkrill <48968101+viktorkrill@users.noreply.github.com>
2022-02-10 13:41:15 -0500

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - i need print Programming is like building a multilingual puzzle
-* return: 0 this should always be in zero
+* main - print the word "Programming is like building a multilingual puzzle"
+* Return: 0 this should always be in zero
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
