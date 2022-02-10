277c91a482497b274e2e3b729dc9ebb32df57432
viktorkrill <48968101+viktorkrill@users.noreply.github.com>
2022-02-10 13:37:34 -0500

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main - This is the exercise that I must perform
+* main - i need print Programming is like building a multilingual puzzle
 * return: 0 this should always be in zero
 */
 int main(void)
