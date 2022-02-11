30df40832fcde927c1537ecd36cd07b195397b94
viktor krill <viktorkrilldj@gmail.com>
2022-02-10 17:19:02 -0800

    el commit 839202

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("Side of a char: %li byte(s)\n", sizeof(char));
-printf("Size of an int: %li byte(s)\n", sizeof(int));
-printf("Size of a long int: %li byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %li byte(s)\n", sizeof(long long int));
-printf("Size of a float: %li byte(s)\n", sizeof(float));
+printf("Side of a char: %2d byte(s)\n", sizeof(char));
+printf("Size of an int: %2d byte(s)\n", sizeof(int));
+printf("Size of a long int: %2d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %2d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %2d byte(s)\n", sizeof(float));
 return (0);
 }
