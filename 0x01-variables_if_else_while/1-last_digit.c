4639f7f19d65eb8739e46815fff86f1518185a61
viktor krill <viktorkrilldj@gmail.com>
2022-02-11 10:07:22 -0800

    el comite

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Greater than or 6
+ * Return: 0  is the value to return
+*/
+int main(void)
+{
+int n;
+int z;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+z = n % 10;
+if (z > 5)
+printf("and is greater than 5\n");
+else if (z == 0)
+printf("and is 0\n");
+else if (z < 6)
+printf("and is less than 6 and not 0\n");
+return (0);
+}
