2e787319ee890d40af5cb663b2ce8f074ef3d275
viktor krill <viktorkrilldj@gmail.com>
2022-02-11 10:16:12 -0800

    comiteano

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,10 +13,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 z = n % 10;
 if (z > 5)
-printf("and is greater than 5\n");
+printf("and is greater than 5\n", z);
 else if (z == 0)
-printf("and is 0\n");
+printf("and is 0\n", z);
 else if (z < 6)
-printf("and is less than 6 and not 0\n");
+printf("and is less than 6 and not 0\n", z);
 return (0);
 }
