e0e73f5b5a22eb248ba795f00c2f890af17e95a1
viktor krill <viktorkrilldj@gmail.com>
2022-02-11 16:23:26 -0800

    comiteando

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Desde la a hasta la z 
+ * vamos con toda
+ * Return: 0 no retorna nada yayayajuuuu
+ */
+int main(void)
+{
+char f;
+for (f = 'a'; f <= 'z'; f++)
+putchar(f);
+putchar('\n');
+return (0);
+}
