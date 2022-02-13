e7af015e853c13097f763b1077f1414de5c8d58c
viktor krill <viktorkrilldj@gmail.com>
2022-02-13 14:22:20 -0800

    comiteando

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - imprime del 1 al 10
+ * go to the 1 -10
+ * Return: 0 for ever return
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	printf("%d", n);
+	printf("\n");
+
+	return (0);
+}
