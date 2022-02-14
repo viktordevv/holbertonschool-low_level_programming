8445c07da918037ede685b3bb21628da7376ce7a
Viktor <4640@holbertonschool.com>
2022-02-13 20:43:15 -0800

    el commit final

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *
+ *
+ */
+int main(void)
+{
+	int p;
+	for (p = '0'; p <= '9';)
+	{
+	if (p <= '9')
+	putchar(p);
+	{
+		putchar(',');
+	putchar(' ');
+	p++;
+	}
+	}
+			return (0);
+}
+
