51fb02b0f56f988e7fcea516a7866dea852617d0
viktorkrill <48968101+viktorkrill@users.noreply.github.com>
2022-02-14 00:08:19 -0500

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- *
- *
+ * main -This prints up to 9
+ * Then of so much if you print
+ * Return:0 there is no way back
  */
 int main(void)
 {
 	int p;
+	
 	for (p = '0'; p <= '9'; p++)
 {	putchar(p);
 	if (p != '9')
