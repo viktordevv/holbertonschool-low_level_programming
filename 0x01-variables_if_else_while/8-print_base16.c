b8616271c9761bde94fdcd696d5c0a4a09b81501
Viktor <4640@holbertonschool.com>
2022-02-13 17:42:29 -0800

    el commit de base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - esto imprime base16
+ * del 0 al 9 y de la a al f
+ * Return: 0 no retorna nadita
+ */
+int main(void)
+{
+	char m;
+	char n;
+
+	for (m = '0'; m <= '9'; m++)
+		putchar(m);
+	for (n = 'a'; n <= 'f'; n++)
+	       putchar(n);
+putchar('\n');
+return (0);
+}
