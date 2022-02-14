12aee1448cb7012541b16e19b3806714bae2d261
Viktor <4640@holbertonschool.com>
2022-02-13 16:41:32 -0800

    el commit de la 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Imprimiendo del 1 al 9
+ * Return: 0 Devolviendo en 0 
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	putchar(n);
+	putchar('\n');
+	return (0);
+}
