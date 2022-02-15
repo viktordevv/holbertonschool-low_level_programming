6ceb27f771d6a5a2e903fee555d2af267b019c85
viktorkrill <48968101+viktorkrill@users.noreply.github.com>
2022-02-15 16:45:24 -0500

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,6 @@
  * print_alphabet_x10 - multiplying by 10
  * Return:0 dont return from main
  */
-
 void print_alphabet_x10(void)
 {
 	char m;
@@ -17,10 +16,6 @@ void print_alphabet_x10(void)
 		_putchar(m);
 		}
 		_putchar('\n');
-<<<<<<< HEAD
 		list++;	
-=======
-		list++;
->>>>>>> e1e0b5960e2dbdf9e786600d5cd27e1084e1ed00
 }
 }
