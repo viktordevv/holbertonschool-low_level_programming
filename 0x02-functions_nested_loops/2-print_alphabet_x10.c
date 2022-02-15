e52c0d2c19dcbf478137581f9d03f7a068ca87a7
viktorkrill <48968101+viktorkrill@users.noreply.github.com>
2022-02-15 09:28:11 -0500

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - multiplying by 10
+ * print_alphabet_x10 - multiplying by 10
  * Return:0 dont return from main
  */
 
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 {
 	char m;
 	int list = 0;
-	
+
 	while (list < 10)
 	{
 		for (m = 'a'; m <= 'z'; m++)
