fb2cd679ac3e892dbf30471510fa45441f571af0
Viktor <4640@holbertonschool.com>
2022-02-15 12:22:56 -0800

    el 10 jeje

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - function that plus 2 integral
+ * @q: first integral
+ * @w: second integral
+ * Return: integral
+ */
+int add(int t, int l)
+{
+	return (t + l);
+}
