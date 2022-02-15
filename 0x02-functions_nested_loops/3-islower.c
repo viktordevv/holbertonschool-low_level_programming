50fa122afe1ced7a41de2fddeb1dd92824c2d577
viktorkrill <48968101+viktorkrill@users.noreply.github.com>
2022-02-15 09:37:11 -0500

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * _islower - de la a-z
- * c - the corrid
+ * c the corrid lowercase
  * Return: 0 no devuelve nada
  */
 int _islower(int c)
-{	
+{
 
 	if (c >= 'a' && c <= 'z')
-		return (1);
+	return (1);
 
 	else
-		return (0);
+	return (0);
 }
