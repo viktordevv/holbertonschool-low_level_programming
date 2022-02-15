6aabc4936e8c26cd3ee5f30ca87e95e0f628f913
Viktor <4640@holbertonschool.com>
2022-02-15 13:34:54 -0800

    the finish

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_to_98 - print numbers
+ * @n: the 1 to 98
+ */
+void print_to_98(int a)
+{
+	if (a > 98)
+	
+	for (; a > 98; a--)
+		{
+		printf("%d, ", a);
+		}
+	
+	else if (a < 98)
+	{
+	for (; a < 98; a++)
+		{
+		printf("%d, ", a);
+		}
+	}
+		printf("%d\n", a);
+}
