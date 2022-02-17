72bbe5248d082a9bb02371b3e4da258747b215d5
Viktor <4640@holbertonschool.com>
2022-02-17 10:47:33 -0800

    this is commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - this is main funtion
+ * @a: this is integer
+ * @b: this is other number
+ * Return: Always a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
