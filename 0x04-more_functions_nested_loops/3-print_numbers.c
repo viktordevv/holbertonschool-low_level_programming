5dc557aeb385b3f6764248e84cf9245eb34c8cb8
Viktor <4640@holbertonschool.com>
2022-02-17 11:16:04 -0800

    the commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_numbers - this is the main funtion of project
+ */
+void print_numbers(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	_putchar(a);
+	_putchar('\n');
+}
