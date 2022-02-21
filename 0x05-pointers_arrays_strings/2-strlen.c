c7f92c8e95b628d35c4af30786f968a4a02614dd
Viktor <4640@holbertonschool.com>
2022-02-21 12:07:13 -0800

    this is comi

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - this is main
+ * @s: this is string
+ * Return: 1  this is return funtion
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		l++;
+	}
+	return (l);
+}
