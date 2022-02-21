abf73d7c80843bdfe547c0995864581ea8386a51
Viktor <4640@holbertonschool.com>
2022-02-21 13:41:57 -0800

    the commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int lo = 0;
+	int p;
+	
+	while (*s != '\0')
+	{
+		lo++;
+		s++;
+	}
+		s--;
+		for (p = lo; p > 0; p--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+		_putchar('\n');
+}
