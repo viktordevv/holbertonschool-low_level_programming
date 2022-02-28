74ae4209d48a241e126a96f1c5ec6ae0b8a0dc50
Viktor <viktorkrilldj@gmail.com>
2022-02-28 17:27:50 -0500

    commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _memset - this is main
+ * *s: this is point
+ * @b: this is b funt
+ * @n: rhis is n funt
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+    unsigned int i;
+    for (i = 0; i < n; i++)
+    s[i] = b;
+    return (s);
+}
