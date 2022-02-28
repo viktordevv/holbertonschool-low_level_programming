3e2482144cb90bdc88993d619e0697315494f6db
Viktor <viktorkrilldj@gmail.com>
2022-02-28 18:31:01 -0500

    commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - this is main funt
+ * @dest: this is char
+ * @src: this is a src
+ * @n: this is n funt
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    unsigned int i = 0;
+
+    for (i = 0; i < n; i++)
+    {
+        dest[i] = src[i];
+    }
+    return (dest);
+}
