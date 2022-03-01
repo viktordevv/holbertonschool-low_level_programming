5f84b537fddcce81600c204c1b37f6d71a68978f
viktorkrill <48968101+viktorkrill@users.noreply.github.com>
2022-02-28 20:28:14 -0500

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,14 +1,14 @@
 #include "main.h"
-
 /**
-* _strchr - funcion que retorne llo
-* @a: variable
-* @c: variable
+* _strchr - this is funtion main
+* @s: this is a funtion
+* @c: this is a c funcion
 * Return: 0
 */
 char *_strchr(char *s, char c)
 {
 int x;
+  
 for (x = 0; s[x] != '\0'; x++)
 {
 if (s[x] == c)
