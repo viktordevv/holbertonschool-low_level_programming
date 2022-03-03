716c69856a7c78d9326360ea6d7251eb6c618e08
viktorkrill <48968101+viktorkrill@users.noreply.github.com>
2022-03-02 21:01:37 -0500

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,18 +2,17 @@
 /**
  * factorial - This is factorial main
  * @n: this is n funtion
-* Return: n
+ * Return: n
  */
-
 int factorial(int n)
 {
-        if (n < 0)
-        {
-                return (-1);
-        }
-        else if (n == 0 || n == 1)
-        {
-                return (1);
-        }
-        return (n * factorial(n - 1));
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 0 || n == 1)
+{
+return (1);
+}
+return (n * factorial(n - 1));
 }
