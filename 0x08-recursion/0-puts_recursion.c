83adda8cc48d4ff97f42b2393b65341ae03077ad
viktorkrill <48968101+viktorkrill@users.noreply.github.com>
2022-03-02 20:39:55 -0500

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,13 +5,13 @@
  */
 void _puts_recursion(char *s)
 {
-    if (*s)
-    {
-        _putchar(*s);
-        _puts_recursion(s + 1);
-    }
-    else
-    {
-        _putchar('\n');
-    }
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+{
+_putchar('\n');
+}
 }
