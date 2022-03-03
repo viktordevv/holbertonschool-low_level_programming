e77f9f777ae13221ca2220f1a6e6232a6d90a92e
viktorkrill <48968101+viktorkrill@users.noreply.github.com>
2022-03-02 20:55:15 -0500

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,12 +6,12 @@
  */
 int _strlen_recursion(char *s)
 {
-    int len = 0;
-    
-    if (*s)
-    {
-        len++;
-        len += _strlen_recursion(s + 1);
-    }
-    return (len);
+int len = 0;
+
+if (*s)
+{
+len++;
+len += _strlen_recursion(s + 1);
+}
+return (len);
 }
