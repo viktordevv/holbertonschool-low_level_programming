de8b0e72fe6648f42c6ddbb91e31ca6e98f7d49a
viktorkrill <48968101+viktorkrill@users.noreply.github.com>
2022-03-04 16:26:21 -0500

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,6 +4,8 @@
  * main - this is funtion main
  * @argc: this is argc
  * @argv: this is argv
+ * @a: is a funtion
+ * @b: is b funtion
  **/
 int main(int argc, char *argv[])
 {
@@ -18,5 +20,6 @@ a = atoi(argv[1]);
 b = atoi(argv[2]);
 result = a * b;
 printf("%d\n", result);
+
 return (0);
 }
