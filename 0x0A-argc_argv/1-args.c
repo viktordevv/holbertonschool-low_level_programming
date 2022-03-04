585e20e1e9c8a0f6f19f642bdfec5ea7467c8d95
Viktor <viktorkrilldj@gmail.com>
2022-03-04 15:31:50 -0500

    NARGAS

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - this is funtion main
+ * @argc: this is argc
+ * @argv: this is argv
+ *
+ * Return: Always 0
+ */
+int main(int argc, char __attribute__((unused)) * argv[])
+{
+    printf("%d\n", --argc);
+    return (0);
+}
