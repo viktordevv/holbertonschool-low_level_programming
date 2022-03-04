8fc0251d7d29c9ab7f917b143b5f4deec04080f9
viktorkrill <48968101+viktorkrill@users.noreply.github.com>
2022-03-04 13:27:19 -0500

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * @brief
- *
- *
+ * main - this is funtion main
+ * @argc: this is argc
+ * @argv: this is argv
+ * Return: 0
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-    printf("%s\n", argv[0]);
-
-    return (0);
+printf("%s\n", argv[0]);
+return (0);
 }
