1b2506019b0bd8d07840f7fa5509254d52000967
viktorkrill <48968101+viktorkrill@users.noreply.github.com>
2022-03-04 15:33:15 -0500

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,11 +3,10 @@
  * main - this is funtion main
  * @argc: this is argc
  * @argv: this is argv
- *
  * Return: Always 0
  */
 int main(int argc, char __attribute__((unused)) * argv[])
 {
-    printf("%d\n", --argc);
-    return (0);
+ printf("%d\n", --argc);
+ return (0);
 }
