23cd65d5be5b448755648df2b6ad588afe3dc359
Viktor <viktorkrilldj@gmail.com>
2022-03-06 14:08:54 -0500

    Ststics

--- 0x09-static_libraries/create_static_lib.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-<<<<<<< HEAD
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
-=======
-  gcc -Wall -pedantic -Werror -Wextra -c *.c
-  ar -rc liball.a *.o
->>>>>>> 19bb91d14c09adf893727cb2c75dd87d59c9009a
