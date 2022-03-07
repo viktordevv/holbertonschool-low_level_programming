e41117a49e5f2ef10be2a6ff5749df9c390cfdb9
viktorkrill <48968101+viktorkrill@users.noreply.github.com>
2022-03-07 13:40:57 -0500

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,28 +1,22 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * @brief Create a array object
- * 
- * @param size 
- * @param c 
- * @return char* 
+ * create_array - this is main funtion
+ * @size: this is size funtion
+ * @c: this is c funtion
  */
-
 char *create_array(unsigned int size, char c);
 {
-    char *array;
-    unsigned int p;
+char *array;
+unsigned int p;
 
-    str = malloc(sizeof(char) * size);
+str = malloc(sizeof(char) * size);
 
 if (size == 0 || array == NULL)
 return (NULL);
 
 for (size_t p = 0; p < count; p++)
-    array[p] = c;
-    
-    return (array);
-
+array[p] = c;
+return (array);
 }
