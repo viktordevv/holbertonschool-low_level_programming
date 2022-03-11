b365cc7b82e1d7c63a1eb6d9c4dd987b55ccb63d
Viktor <viktorkrilldj@gmail.com>
2022-03-11 08:40:08 -0500

    first commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - this is main funtion
+ * @b: this is b funtion 
+ * @return a 
+ */
+
+void *malloc_checked(unsigned int b)
+{
+    void *a;
+
+    a = malloc(b);
+    if (a == NULL)
+    {
+     exit(98);
+    }
+    else
+    {
+        return (a);
+    }
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
