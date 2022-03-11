e64230169db2851fb8b1c77598cf4024813af598
viktorkrill <48968101+viktorkrill@users.noreply.github.com>
2022-03-11 08:43:14 -0500

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,24 +1,22 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * malloc_checked - this is main funtion
- * @b: this is b funtion 
- * @return a 
+ * @b: this is b funtion
+ * @return a
  */
-
 void *malloc_checked(unsigned int b)
 {
-    void *a;
+void *a;
 
-    a = malloc(b);
-    if (a == NULL)
-    {
-     exit(98);
-    }
-    else
-    {
-        return (a);
-    }
+a = malloc(b);
+if (a == NULL)
+{
+exit(98);
+}
+else
+{
+return (a);
+}
 }
