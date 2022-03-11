7b746b7b40dad402996e48e080ea938881ea9478
Viktor <viktorkrilldj@gmail.com>
2022-03-11 14:37:31 -0500

    commit

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - this is main funtion
+ * @nmemb: this is nmemb
+ * @size: this is size
+ * Return: str
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int l;
+	char *str;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	str = malloc(nmemb * size);
+	if (!str)
+	{
+		return (NULL);
+	}
+	for (l = 0; l < nmemb * size; l++)
+	{
+		str[l] = 0;
+	}
+	return (str);
+}
