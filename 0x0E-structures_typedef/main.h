2bec50545104901c4bf80dd2dd235eb8f1accb4a
Viktor <viktorkrilldj@gmail.com>
2022-03-14 15:19:30 -0500

    hello dog

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - THE DOGGY
+ * @name: name
+ * @age: age
+ * @owner: owner
+ **/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
