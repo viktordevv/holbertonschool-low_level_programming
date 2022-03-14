e6727227742991fcde013e3b0769bda6587c24ba
viktorkrill <48968101+viktorkrill@users.noreply.github.com>
2022-03-14 13:44:32 -0500

    Update 3-main.c

--- 0x0D-preprocessor/3-main.c
+++ 0x0D-preprocessor/3-main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include "3-function_like_macro.h"
-#include "3-function_like_macro.h"
 /**
  * main - check the code
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
