64fa119d9d668a2389f7fbd44752736737e3b6aa
Viktor <viktorkrilldj@gmail.com>
2022-03-15 13:59:54 -0500

    the finish

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_dog - this is main funtion
+ * @d: this is d funtion
+ */
+void free_dog(dog_t *d)
+{
+if (d)
+
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
