0c823eb958fb41ccd04a41f44d274ac66679a9bf
Viktor <viktorkrilldj@gmail.com>
2022-03-16 21:06:28 -0500

    commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <function_pointers.h>
+/**
+ * print_name - this is a main
+ * @name: this is name
+ * @f: this is f
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL || f == NULL)
+return;
+f(name);
+}
