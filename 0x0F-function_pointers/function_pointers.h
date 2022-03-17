1d02c4a26488099a235924495315ed856fd4cd1a
viktorkrill <48968101+viktorkrill@users.noreply.github.com>
2022-03-17 15:54:46 -0500

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,10 +2,11 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stddef.h>
 
-int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+int _putchar(char c);
 
 #endif
