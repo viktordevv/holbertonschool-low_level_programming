a6f870b6c0b4a57d17a59ad3c99d9cc758d49075
Viktor <viktorkrilldj@gmail.com>
2022-03-18 18:43:54 -0500

    commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,21 +2,18 @@
 #include <stdio.h>
 /**
  * sum_them_all
- * @n: 
+ * @n: this is n
  * Return: 0 
  */
-
-int sum_them_all(const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...)
 {
     unsigned int c;
     int sum = 0;
     va_list l;
 
     va_start(l, n);
-
     for (c = 0; c < n; c++)
         sum += va_arg(l, int);
-
     va_end(l);
     return (sum);
 }
