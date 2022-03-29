2d42aad00b54e0e5519909746b819ca6aec5843d
Viktor <viktorkrilldj@gmail.com>
2022-03-29 16:16:42 -0500

    the commit 5

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * @add_node -this is main function
+ * @head:this is head funtion
+ * @str: this is string
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *node;
+
+	node = malloc(sizeof(list_t));
+	if (node == NULL)
+	return (NULL);
+	node->str = strdup(str);
+	node->len = strlen(str);
+	node->next = *head;
+	*head = node;
+	return (node);
+}
