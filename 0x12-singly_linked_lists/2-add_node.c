257c69b0fe1c5aec40fe6da080958b2d9b01f0f5
Viktor <48968101+viktorkrill@users.noreply.github.com>
2022-03-29 16:20:29 -0500

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,17 +3,18 @@
  * @add_node -this is main function
  * @head:this is head funtion
  * @str: this is string
+ * Return: 0
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *node;
+list_t *node;
 
-	node = malloc(sizeof(list_t));
-	if (node == NULL)
-	return (NULL);
-	node->str = strdup(str);
-	node->len = strlen(str);
-	node->next = *head;
-	*head = node;
-	return (node);
+node = malloc(sizeof(list_t));
+if (node == NULL)
+return (NULL);
+node->str = strdup(str);
+node->len = strlen(str);
+node->next = *head;
+*head = node;
+return (node);
 }
