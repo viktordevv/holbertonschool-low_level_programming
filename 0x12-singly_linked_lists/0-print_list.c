9c106fdd73a2d179a8e9eab476ba86f34bdf7427
Viktor <48968101+viktorkrill@users.noreply.github.com>
2022-03-29 08:49:45 -0500

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,11 +2,9 @@
 
 /**
  * print_list - this is main funtion
- *
  * @h: this is list h
  * Return: sum
  */
-
 size_t print_list(const list_t *h)
 {
     size_t sum = 0;
@@ -15,7 +13,6 @@ size_t print_list(const list_t *h)
     {
        if (!h->str)
             printf("[0] (nil)\n");
-
         else
             printf("[%u] %s\n", h->len, h->str);
               h = h->next;
