04d251110353d2f60219fb503d5d7b89024a42b0
Viktor <48968101+viktorkrill@users.noreply.github.com>
2022-03-29 08:51:33 -0500

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,16 +6,16 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t sum = 0;
+size_t sum = 0;
 
-    while (h)
-    {
-       if (!h->str)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
-              h = h->next;
-            sum++;
-    }
-    return (sum);
+while (h)
+{
+if (!h->str)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+sum++;
+}
+return (sum);
 }
