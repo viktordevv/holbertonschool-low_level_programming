2689112b564fe2cfc0dd57047b0df125e39049db
Viktor <48968101+viktorkrill@users.noreply.github.com>
2022-03-29 19:14:37 -0500

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,22 +8,22 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *nodo;
-	list_t *last = *head;
+list_t *nodo;
+list_t *last = *head;
 
-	nodo = malloc(sizeof(list_t));
-	if (nodo == NULL)
-	return (NULL);
-	nodo->str = strdup(str);
-	nodo->len = strlen(str);
-	nodo->next = NULL;
-	if (*head == NULL)
-	{
-	*head = nodo;
-	return (nodo);
-	}
-	while (last->next != NULL)
-	last = last->next;
-	last->next = nodo;
-	return (nodo);
+nodo = malloc(sizeof(list_t));
+if (nodo == NULL)
+return (NULL);
+nodo->str = strdup(str);
+nodo->len = strlen(str);
+nodo->next = NULL;
+if (*head == NULL)
+{
+*head = nodo;
+return (nodo);
+}
+while (last->next != NULL)
+last = last->next;
+last->next = nodo;
+return (nodo);
 }
