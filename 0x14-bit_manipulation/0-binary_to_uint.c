a147fe69a89ab28906cf230479a69859b0224581
Viktor <48968101+viktorkrill@users.noreply.github.com>
2022-04-04 14:25:05 -0500

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,22 +6,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int sum;
+int i;
+unsigned int sum;
 
-	sum = 0;
-	if (!b)
-		return (0);
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-	}
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		sum <<= 1;
-		if (b[i] == '1')
-			sum += 1;
-	}
-	return (sum);
+sum = 0;
+if (!b)
+return (0);
+for (i = 0; b[i] != '\0'; i++)
+{
+if (b[i] != '0' && b[i] != '1')
+return (0);
+}
+for (i = 0; b[i] != '\0'; i++)
+{
+sum <<= 1;
+if (b[i] == '1')
+sum += 1;
+}
+return (sum);
 }
