94b49d0b0639b1eebe7b14dfdca344a8abf93ba0
Viktor <viktorkrilldj@gmail.com>
2022-04-04 18:07:04 -0500

    commi

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,15 +7,16 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+      while (index > 64)
+    {
+        return (-1);
+    }
+
     n >>= index;
     if ((n & 1) == 1)
         return (1);
     else
     {
         return (0);
-    }
-    while (index > 64)
-    {
-        return (-1);
     }
 }
