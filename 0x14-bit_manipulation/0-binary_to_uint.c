69ceb5b787e74fd2c0e0c32df3b83d1bc80e80d4
Viktor <viktorkrilldj@gmail.com>
2022-04-04 14:20:11 -0500

    the first

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * binary_to_uint - this is main
+ * @b: this is char
+ * Return: sum
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int sum;
+
+	sum = 0;
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		sum <<= 1;
+		if (b[i] == '1')
+			sum += 1;
+	}
+	return (sum);
+}
