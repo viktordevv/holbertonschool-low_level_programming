889dc5ebc9611d0a98f3c367ad37e889b7e44539
Viktor <viktorkrilldj@gmail.com>
2022-04-04 19:25:02 -0500

    commit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * set_bit - this is main funtion
+ * @n: this is funtion
+ * @index: this is index
+ * Return: 0
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+    unsigned int p = 0;
+
+    if (n == 0 && index < 63)
+    {
+        return (-1);
+    }
+
+    {
+        p = 1 << index;
+        *n = (*n | p);
+        return (1);
+    }
+}
