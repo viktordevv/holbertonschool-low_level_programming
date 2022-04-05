92afd1c0fe11c04ffd0c12e3f98931c2c5b9579e
Viktor <viktorkrilldj@gmail.com>
2022-04-05 15:54:09 -0500

    file 2

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,24 +7,24 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int writebuff, readbuff, fd;
-	char *buffptr;
+    int writebuff, readbuff, fd;
+    char *buffptr;
 
-	if (filename == NULL)
-		return (0);
-	buffptr = malloc(sizeof(char) * letters);
-	if (buffptr == NULL)
-		return (0);
-	fd = open(filename, O_RDWR);
-	if (fd == -1)
-		return (0);
-	readbuff = read(fd, buffptr, letters);
-	if (readbuff == -1)
-		return (0);
-	writebuff = write(STDOUT_FILENO, buffptr, readbuff);
-	if (writebuff == -1)
-		return (0);
-	close(fd);
-	free(buffptr);
-	return (writebuff);
+    if (filename == NULL)
+        return (0);
+    buffptr = malloc(sizeof(char) * letters);
+    if (buffptr == NULL)
+        return (0);
+    fd = open(filename, O_RDWR);
+    if (fd == -1)
+        return (0);
+    readbuff = read(fd, buffptr, letters);
+    if (readbuff == -1)
+        return (0);
+    writebuff = write(STDOUT_FILENO, buffptr, readbuff);
+    if (writebuff == -1)
+        return (0);
+    close(fd);
+    free(buffptr);
+    return (writebuff);
 }
